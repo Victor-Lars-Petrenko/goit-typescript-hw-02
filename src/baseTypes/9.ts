9c1d032d869d03708e28e363ba89bdbe39b55808
Victor Petrenko <138097657+Victor-Lars-Petrenko@users.noreply.github.com>
2024-03-01 16:26:38 +0200

    done-1

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -1,23 +1,38 @@
 /*
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
+type Page = {
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: "open" | "close";
+};
 
-const page1 = {
-  title: 'The awesome page',
-  likes: 100,
-  accounts: ['Max', 'Anton', 'Nikita'],
-  status: 'open',
+type Details = {
   details: {
-    createAt: new Date('2021-01-01'),
-    updateAt: new Date('2021-05-01'),
-  }
-}
+    createAt: Date;
+    updateAt: Date;
+  };
+};
 
-const page2 = {
-  title: 'Python or Js',
+type DetailedPage = Page & Details;
+
+const page1: DetailedPage = {
+  title: "The awesome page",
+  likes: 100,
+  accounts: ["Max", "Anton", "Nikita"],
+  status: "open",
+  details: {
+    createAt: new Date("2021-01-01"),
+    updateAt: new Date("2021-05-01"),
+  },
+};
+
+const page2: Page = {
+  title: "Python or Js",
   likes: 5,
-  accounts: ['Alex'],
-  status: 'close',
-}
+  accounts: ["Alex"],
+  status: "close",
+};
 
 export {};
